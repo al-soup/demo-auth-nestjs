b08fbe192b532cb154e4f2b615f7e0e0936635e4
al-soup <alex@kraeuchi.io>
2020-06-14 18:39:10 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors()
   app.setGlobalPrefix('api');
+  
   const port = 3000;
   await app.listen(port);
   console.log(`Server started on port ${port}`);
